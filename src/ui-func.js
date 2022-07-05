a07663b5e9ea6132618061e385d34b9c41026778
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-05 11:57:43 -0700

    Text portion functions.
    Co-authored-by:Peter Vang <vang_peter06@yahoo.com>
    Co-authored-by:  PJ Perry <pjperry28@gmail.com>

--- src/ui-func.js
+++ src/ui-func.js
@@ -1,19 +1,102 @@
-import bizFunc from './js/biz-func.js';
+// import bizFunc from './js/biz-func.js';
 import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+
 
 $(document).ready(function() {
-  $("form#example-form").submit(function(event) {
-    event.preventDefault();
-    const numbers = $("#number").val();
-    const text = $("#text").val();
-    const exampleText = exampleTestFunction(numbers, text);
-    console.log(exampleText);
-    $("#output").text(exampleText);
-  
-    function logReset() {
-    }
-  });
+ $("#startButton").click(function(){
+ $(".hallway").show();
+ $(".story").hide();
+ });
+ $("#hallwayButton").click(function() {
+  let hallwayAction = $("#hallway").val();
+  if (hallwayAction.toLowerCase() === "move to attic") {
+    $(".attic").show();
+    $(".hallway").hide();
+  } else if (hallwayAction.toLowerCase() === "move to cellar") {
+    $(".cellar").show();
+    $(".hallway").hide();
+  } else if (hallwayAction.toLowerCase() === "move to study") {
+    $(".study").show();
+    $(".hallway").hide();
+  } else if (hallwayAction.toLowerCase() === "move to parlor") {
+    $(".parlorLocked").show();
+    $(".hallway").hide();
+  } else if (hallwayAction.toLowerCase() === "move to study") {
+    $(".study").show();
+    $(".hallway").hide();
+  } else {
+    $("#hallwayError").show();
+  }
+});
+
+$("#studyButton").click(function(){
+  let studyAction = $("#study").val();
+  if (studyAction.toLowerCase() === "move to hallway"){
+    $(".hallway").show();
+    $(".study").hide();
+  } else{
+    $("#studyError").show();
+  }
+});
+
+$("#cellarButton").click(function() {
+  let cellarAction = $("#cellar").val();
+  if (cellarAction.toLowerCase() === "move to hallway") {
+    $(".hallway").show();
+    $(".cellar").hide();
+  } else {
+    $("#cellarError").show();
+  }
+});
+
+$("#atticButton").click(function() {
+  let atticAction = $("#attic").val();
+  if (atticAction.toLowerCase() === "move to hallway") {
+    $(".hallway").show();
+    $(".attic").hide();
+  } else if (atticAction.toLowerCase() === "pick up laser gun") {
+    $("#pickUpLaser").show();
+  } else {
+    $("#atticError").show();
+  }
+});
+
+$("#parlorLockedButton").click(function() {
+  let parlorAction = $("#parlor").val();
+  if (parlorAction.toLowerCase() === "move to hallway") {
+    $(".hallway").show();
+    $(".parlorLocked").hide();
+  } else if (parlorAction.toLowerCase() === "use laser gun") {
+    $(".parlorUnlocked").show();
+    $(".parlorLocked").hide();
+  } else if (parlorAction.toLowerCase() === "move to attic") {
+    $(".attic").show();
+    $(".parlorLocked").hide();
+  } else if (parlorAction.toLowerCase() === "move to cellar") {
+    $(".cellar").show();
+    $(".parlorLocked").hide();
+  } else if (parlorAction.toLowerCase() === "move to study") {
+    $(".study").show();
+    $(".parlorLocked").hide()
+  } else {
+    $("#parlorError").show();
+  }
+});
+
+$("#parlorUnlockedButton").click(function() {
+  let parlorUnlockedAction = $("#parlorUnlocked").val();
+  if (parlorUnlockedAction.toLowerCase() === "move to hallway") {
+    $(".hallway").show();
+    $(".parlorUnlocked").hide();
+  } else if (parlorUnlockedAction.toLowerCase() === "use laser gun") {
+    $(".attack").show();
+    $(".parlorUnlocked").hide();
+  } else {
+    $("#parlorUnlockedError").show();
+  }
+});
 });
