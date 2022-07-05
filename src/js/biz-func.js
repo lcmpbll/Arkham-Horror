0af89fdb1d9e7fc2465a67b6a32a08c4718a020b
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-05 16:42:43 -0700

    Add pick UpLaser function.
    Co-authored-by: PJ Perry <Jamesp92@users.noreply.github.com>
    Co-authored-by: vangpeter06 <vangpeter06@users.noreply.github.com>
    Co-authored-by: bpgoodrow <bpgoodrow@users.noreply.github.com>

--- src/js/biz-func.js
+++ src/js/biz-func.js
@@ -1,8 +1,34 @@
-// import $ from 'jquery';
+//let character = new Character();
+export default class Character {
 
- const exampleTestFunction = () => {
-  console.log('hi there, from the deck');
-  return 'hi there, from the deck';
-};
+  constructor () {
+  this.inventory = [];
+  this.health = 10;
+  this.aim = 0;
+ }
+  pickUpLaser() {
+  let laserGun = {"name" : "laser gun"};
+  this.inventory.push(laserGun)
+ }
+  hasLaser() {
+  for (let i =0; i <= this.inventory.length; i ++)
+    { this.inventory.includes('laser gun');
+    // this hasLaser hasn't work yet. Needs work.
+    }
+  }
 
-export default exampleTestFunction;
+  increaseStats() {
+  this.aim += 1;
+  this.health += 1;
+}
+
+shootLaser() {
+  if (this.aim === 0) {
+   this.damage = 1 + Math.floor(Math.random() * 6);
+  } else {
+   this.damage = 3 + Math.floor(Math.random() * 6);
+  } 
+  return this.damage;
+}
+
+}
