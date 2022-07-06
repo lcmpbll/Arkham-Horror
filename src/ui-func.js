d41899ccac0914d65c61e8433fde760fcdc34c85
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-06 14:18:01 -0700

    update styling and add new room pathing to JS
    Co-authored-by: PJ Perry <pjperry28@gmail.com>

--- src/ui-func.js
+++ src/ui-func.js
@@ -5,29 +5,58 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
-
 $(document).ready(function() {
+
+  $("#menuButton").click(function() {
+    $(".story").show();
+    $(".menu").hide();
+  })
+
   $("#startButton").click(function(){
-  $(".hallway").show();
+  $(".alley").show();
   $(".story").hide();
   });
+
+  $("#alleyButton").click(function() {
+    let alleyAction = $("#alley").val();
+    if (alleyAction.toLowerCase() === "move to house") {
+      $(".hallway").show();
+      $(".alley").hide();
+    } else if (alleyAction.toLowerCase() === "call the authorities"){
+      $(".authorities").show();
+      $(".alley").hide()
+      $("#alleyError").hide();
+    } else $("#alleyError").show();
+  });
+
+  $("#authoritiesButton").click(function() {
+    $(".alley").show();
+    $(".authorities").hide();
+ });
+
   $("#hallwayButton").click(function() {
     let hallwayAction = $("#hallway").val();
     if (hallwayAction.toLowerCase() === "move to attic") {
       $(".attic").show();
       $(".hallway").hide();
+      $("#hallwayError").hide();
     } else if (hallwayAction.toLowerCase() === "move to cellar") {
       $(".cellar").show();
       $(".hallway").hide();
+      $("#hallwayError").hide();
+      $("#killRats").hide();
     } else if (hallwayAction.toLowerCase() === "move to study") {
       $(".study").show();
       $(".hallway").hide();
+      $("#hallwayError").hide();
     } else if (hallwayAction.toLowerCase() === "move to parlor") {
       $(".parlorLocked").show();
       $(".hallway").hide();
+      $("#hallwayError").hide();
     } else if (hallwayAction.toLowerCase() === "move to study") {
       $(".study").show();
       $(".hallway").hide();
+      $("#hallwayError").hide();
     } else {
       $("#hallwayError").show();
     }
@@ -38,6 +67,7 @@ $(document).ready(function() {
     if (studyAction.toLowerCase() === "move to hallway"){
       $(".hallway").show();
       $(".study").hide();
+      $("#studyError").hide();
     } else{
       $("#studyError").show();
     }
@@ -48,6 +78,8 @@ $(document).ready(function() {
     if (cellarAction.toLowerCase() === "move to hallway") {
       $(".hallway").show();
       $(".cellar").hide();
+      $("#cellarError").hide();
+      $("#killRats").hide();
     } else if (cellarAction === "use laser gun") {
       $("#killRats").show();
       Character.increaseStats();
@@ -61,6 +93,8 @@ $(document).ready(function() {
     if (atticAction.toLowerCase() === "move to hallway") {
       $(".hallway").show();
       $(".attic").hide();
+      $("#atticError").hide();
+      $("#pickUpLaser").hide();
     } else if (atticAction.toLowerCase() === "pick up laser gun") {
       $("#pickUpLaser").show();
      Character.pickUpLaser();
@@ -69,23 +103,28 @@ $(document).ready(function() {
     }
   });
 
-  $("#parlorLockedButton").click(function() {
+  $("#parlorButton").click(function() {
     let parlorAction = $("#parlor").val();
     if (parlorAction.toLowerCase() === "move to hallway") {
       $(".hallway").show();
       $(".parlorLocked").hide();
+      $("#parlorError").hide();
     } else if (parlorAction.toLowerCase() === "use laser gun" ) {
       $(".parlorUnlocked").show();
       $(".parlorLocked").hide();
+      $("#parlorError").hide();
     } else if (parlorAction.toLowerCase() === "move to attic") {
       $(".attic").show();
       $(".parlorLocked").hide();
+      $("#parlorError").hide();
     } else if (parlorAction.toLowerCase() === "move to cellar") {
       $(".cellar").show();
       $(".parlorLocked").hide();
+      $("#parlorError").hide();
     } else if (parlorAction.toLowerCase() === "move to study") {
       $(".study").show();
       $(".parlorLocked").hide();
+      $("#parlorError").hide();
     } else {
       $("#parlorError").show();
     }
@@ -96,11 +135,38 @@ $(document).ready(function() {
     if (parlorUnlockedAction.toLowerCase() === "move to hallway") {
       $(".hallway").show();
       $(".parlorUnlocked").hide();
+      $("#parlorUnlockedError").hide();
     } else if (parlorUnlockedAction.toLowerCase() === "use laser gun") {
       $(".attack").show();
       $(".parlorUnlocked").hide();
-    } else {
+      $("#parlorUnlockedError").hide();
+    }
+    else if (parlorUnlockedAction.toLowerCase() === "move to parlor") {
+      $(".attack").hide();
+      $(".parlorInterior").show();
+      $(".parlorUnlocked").hide();
+      $("#parlorUnlockedError").hide();
+    }
+    else {
       $("#parlorUnlockedError").show();
     }
   });
+
+  $("#parlorButton").click(function() {
+    let parlorUnlockedAction = $("#parlorUnlocked").val();
+    if (parlorUnlockedAction.toLowerCase() === "move to hallway") {
+      $(".hallway").show();
+      $(".parlorUnlocked").hide();
+      $("#parlorUnlockedError").hide();
+    } else if (parlorUnlockedAction.toLowerCase() === "use laser gun") {
+      $(".attack").show();
+      $(".parlorUnlocked").hide();
+      $("#parlorUnlockedError").hide();
+    }
+    else {
+      $("#parlorUnlockedError").show();
+    }
+  });
+
+ 
 });
