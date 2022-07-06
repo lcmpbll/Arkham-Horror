2b4e4318be96e86d480068ae994be9bb424dfb16
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-06 09:23:36 -0700

    Start testing.
    Co-authored-by: vangpeter06 <vangpeter06@users.noreply.github.com>

--- src/js/biz-func.js
+++ src/js/biz-func.js
@@ -5,19 +5,17 @@ export default class Character {
   this.damage = damage;
   this.inventory = [];
   this.health = 10;
+  this.laserPossession = false;
+  
   this.aim = 0;
  }
   pickUpLaser() {
   let laserGun = {"name" : "laser gun"};
+  this.laserPossession = true;
+  
   this.inventory.push(laserGun)
  }
-  hasLaser() {
-  for (let i =0; i <= this.inventory.length; i ++)
-    { this.inventory.includes('laser gun');
-    // this hasLaser hasn't work yet. Needs work.
-    }
-  }
-
+  
   increaseStats() {
   this.aim += 1;
   this.health += 1;
