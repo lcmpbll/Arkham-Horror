31dc653f88c9ddb4d80136f959b5593ebca5eb8a
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-06 10:20:05 -0700

    All tests passing and full character business function coverage.

--- __tests__/biz-func.test.js
+++ __tests__/biz-func.test.js
@@ -6,25 +6,61 @@ describe("Character", () => {
 
   beforeEach(() => {
     newChar = new Character();
- 
+  });
 
     test("should create a character whose health is equal to 10", () => {
-      const newChar = new Character();
+      
       expect(newChar.health).toEqual(10);
     
     });
     test("should create a character whose this.laserPosession is equal to false", () => {
-      const newChar = new Character();
+      
       expect(newChar.laserPossession).toEqual(false);
       
     });
+
     test("should create a character whose this.inventory is empty", () => {
-      const newChar = new Character();
+      
       expect(newChar.inventory.length).toEqual(0);
       
     });
+
+    test("should create a character whose this.health is equal to 0", () => {
+      
+      expect(newChar.aim).toEqual(0);
+    });
+
+    test("should pick up laser and confirm true for laserPossession", () => {
+      newChar.pickUpLaser();
+      expect(newChar.laserPossession).toEqual(true);
+    })
+
+    test("should pick up laser and confirm true for laserPossession", () => {
+      newChar.pickUpLaser();
+      expect(newChar.inventory.length).toEqual(1);
+    })
     
+    test("should increase aim and health when increaseStats is run", () => {
+      newChar.increaseStats();
+      expect(newChar.aim).toEqual(1);
+      expect(newChar.health).toEqual(11);
+    })
+
+    test("Should return damage between 1-6 when shootLaser is called", () => {
+      newChar.shootLaser();
+      expect(newChar.damage).toBeGreaterThanOrEqual(1);
+      expect(newChar.damage).toBeLessThanOrEqual(6);
+      
+    })
+
+    test("Should return damage between 3-6 when shootLaser is called", () => {
+      newChar.increaseStats();
+      newChar.shootLaser();
+      expect(newChar.damage).toBeGreaterThanOrEqual(3);
+      expect(newChar.damage).toBeLessThanOrEqual(6);
+      
+    })
 
   })
 
-});
+
