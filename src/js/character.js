4979c53904c7f2f21327c95016a5b060f32d0bcd
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-10 17:21:16 -0700

    Add ammo

--- src/js/character.js
+++ src/js/character.js
@@ -6,9 +6,11 @@ export default class Character {
     this.status = "alive";
     this.aim = 0;
     this.cellar = false;
+    this.ammo = 0;
   }
   pickUpLaser() {
     this.laserPosession = true;
+    this.ammo = 2;
   }
   
   increaseStats() {
@@ -16,11 +18,17 @@ export default class Character {
     this.health += 1;
   }
 
+  findAmmo() {
+    this.ammo += 1;
+  }
+
   shootLaser() {
     if (this.aim === 0) {
       this.damage = Math.floor(Math.random()* (6 + 1));
+      this.ammo -= 1;
     } else {
       this.damage = 3 + Math.floor(Math.random() *(6 - 3 + 1));
+      this.ammo -= 1;
     } 
     return this.damage;
   }
